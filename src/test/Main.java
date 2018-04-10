b4da4a045bc20a5eff505f1247fe14fc51046de8
Dev_serenotus <Dev_serenotus@serenotus>
2018-04-10 13:04:07 +0800

    reset .ignore

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,10 +1,12 @@
 package test;
 
+import tcsr.practice.DemoApp;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!");
+		DemoApp.main(args);
 	}
 
 }
